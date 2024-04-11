ed3f26ce53699f959a28d5cf1f6627be3d56cac8
yuon7 <yuon7@users.noreply.github.com>
2024-04-11 08:12:52 +0000

    deploy: 3f25a7287c55830bba0baf74e639fea483ceb3f3

---
+++ _next/static/XDyT4hQH-tQlE0lLJdpIV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/ee71d7166a5a0856.css","static/chunks/pages/index-68d16e1ec15fd29a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/qHKZQxaXhgMnCHPTxdjj4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/269b4b1dffab8eb8.css","static/chunks/pages/index-5825505e0d8ba677.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

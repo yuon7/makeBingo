3f25a7287c55830bba0baf74e639fea483ceb3f3
1F10230183 <s1F102301831@iniad.org>
2024-04-11 17:11:43 +0900

    jsonをfetchしてロジックを変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,31 @@ import { useEffect, useState } from 'react';
 import styles from './index.module.css';
 
 const App = () => {
-  useEffect;
-  const [board, setBoard] = useState([
+  const [board, setBoard] = useState<number[][]>([
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
   ]);
 
-  const generateRandomNumber = () => Math.floor(Math.random() * 423) + 1;
+  const [musicIds, setMusicIds] = useState<number[]>([]);
+
+  useEffect(() => {
+    fetch('https://sekai-world.github.io/sekai-master-db-diff/musics.json')
+      .then((response) => response.json())
+      .then((data) => {
+        const ids = data.map((item: { id: number }) => item.id);
+        setMusicIds(ids);
+      })
+      .catch((error) => console.error('Error fetching music IDs:', error));
+  }, []);
+
+  const generateRandomNumber = (ids: number[]) => ids[Math.floor(Math.random() * ids.length)];
 
   const generateRandomBoard = () => {
-    const newBoard = board.map((row) => row.map(() => generateRandomNumber()));
-    setBoard(newBoard);
+    if (musicIds.length > 0) {
+      const newBoard = board.map((row) => row.map(() => generateRandomNumber(musicIds)));
+      setBoard(newBoard);
+    }
   };
 
   const generateJacketUrl = (number: number): string => {
@@ -24,20 +37,18 @@ const App = () => {
   return (
     <div className={styles.container}>
       <div className={styles.board}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          {board.map((row, rowIndex) => (
-            <div key={rowIndex} style={{ display: 'flex' }}>
-              {row.map((number, columnIndex) => (
-                <img
-                  key={columnIndex}
-                  src={generateJacketUrl(number)}
-                  alt={`${number}`}
-                  className={styles.cell}
-                />
-              ))}
-            </div>
-          ))}
-        </div>
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} style={{ display: 'flex' }}>
+            {row.map((number, columnIndex) => (
+              <img
+                key={columnIndex}
+                src={generateJacketUrl(number)}
+                alt={`${number}`}
+                className={styles.cell}
+              />
+            ))}
+          </div>
+        ))}
       </div>
       <button className={styles.buttonNeon} onClick={generateRandomBoard}>
         MakeBingo!

a4706c81ce83f10d6c193ffafcb0fe4637184fe7
yuon7 <yuon7@users.noreply.github.com>
2024-04-25 14:40:06 +0000

    deploy: 94eb8e85afcc8fbef1d781e6a4a10b9e8c14b369

--- _next/static/Hyq2mWLbYpEaSqOKOIZO5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/395-da469d695a482d6c.js","static/css/aad7bd0a1960ce5e.css","static/chunks/pages/index-cd953434d43169f1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
